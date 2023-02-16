22a94a280bdc6e34e03fdf473ea0ba5a586de63f
LiterallyEssie325 <olowoyeyeesther@gmail.com>
2023-02-16 10:57:54 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Print a line of C code using function puts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
