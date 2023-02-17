bbf5919b60aec6856fa7122c691a547095486aba
LiterallyEssie325 <olowoyeyeesther@gmail.com>
2023-02-17 15:10:47 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints single digits
+ * in base 16 lowercase
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int s;
+	char l;
+
+	for (s = 0 ; s <= 10 ; s++)
+		putchar(s);
+	for (l = 'a' ; l <= 'f' ; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
