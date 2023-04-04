e9d452e374b75db25ed90b9ba9bb53ce5254b049
LiterallyEssie325 <olowoyeyeesther@gmail.com>
2023-04-04 15:55:35 -0700

    Add

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * *add_nodeint - adds a new mode at the beginning of a linked list
+ * @head: input value
+ * @n: input value
+ *
+ * Return address of the new element otherwise NULL
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *curry;
+
+	curry = malloc(sizeo(listint_t));
+	if (!curry)
+		return (NULL);
+
+	curry->n = n;
+	curry->next = *head;
+	*head = curry;
+	return (curry);
+}
