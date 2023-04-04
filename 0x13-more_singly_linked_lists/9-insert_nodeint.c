2c969c26ba36dc91d21bb722f5bce121de422a6e
LiterallyEssie325 <olowoyeyeesther@gmail.com>
2023-04-04 16:57:15 -0700

    New node Inserted

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * *insert_nodeint_at_index - inserts a new node at a given position
+ * @idx: input value
+ * @head: input value
+ * @n: input value
+ *
+ * Return: address of the new node otherwise, NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *curry;
+	listint *thyme = *head;
+	unsigned int d;
+
+	curry = malloc(sizeof(listint_t));
+	if (!curry || !head)
+		return (NULL);
+
+	curry->n = n;
+	curry->next = NULL;
+
+	if (idx == 0)
+	{
+		curry->next = *head;
+		*head = curry;
+		return (curry);
+	}
+
+	for (d = 0; thyme && d < idx; d++)
+	{
+		if (d == idx - 1)
+		{
+			curry->next = thyme->next;
+			thyme->next = curry;
+			return (curry);
+		}
+		else
+			thyme = thyme->next;
+	}
+	return (NULL);
