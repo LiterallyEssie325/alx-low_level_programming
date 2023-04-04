87d43d60cf720ca8485db524c7a129288b1ea426
LiterallyEssie325 <olowoyeyeesther@gmail.com>
2023-04-04 16:17:02 -0700

    add thyme

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * *add_nodeint_end - adds a new node at the end of a linked list
+ * @head: input value
+ * @n: input value
+ *
+ * Return: address of the new element otherwise, NULL.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *digit;
+	listint_t *vivid = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (!digit)
+		return (NULL);
+	digit->n = n;
+	digit->next = NULL;
+	if (*head == NULL)
+	{
+		*head = digit;
+		return (digit);
+	}
+
+	while (vivid->next)
+		vivid = vivid->next;
+
+	vivid->next = digit;
+	return (digit);
+}
