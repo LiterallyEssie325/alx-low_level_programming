c9018df8c46edbc4bc60bf8ee0c9bc167d11c445
LiterallyEssie325 <olowoyeyeesther@gmail.com>
2023-04-04 18:05:30 -0700

    Summer

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns sum of all the data in the listint_t list
+ * @head: input value
+ *
+ * Return: return all sum otherwise, 0
+ */
+
+int sum_listint(listint_t *head)
+{
+	int plus = 0;
+	listint_t *dail = head;
+
+	while (dail)
+	{
+		plus += dail->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
