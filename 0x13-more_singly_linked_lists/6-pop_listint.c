f0b958cb3c3c95b0c670662b5de21e0ce73d7b00
LiterallyEssie325 <olowoyeyeesther@gmail.com>
2023-04-04 17:36:52 -0700

    Burst

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a linked list
+ * @head: input value
+ *
+ * Return: the deleted node otherwise, 0
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *thyme;
+	int s;
+
+	if (!head || !*head)
+		return (0);
+
+	s = (*head)->n;
+	thyme = (*head)->next;
+	free(*head);
+	*head = thyme;
+	return (s);
