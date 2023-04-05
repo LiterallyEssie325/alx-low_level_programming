b7dba5cfcbccc91391c8d753a41241ed1283a87a
LiterallyEssie325 <olowoyeyeesther@gmail.com>
2023-04-04 17:28:11 -0700

    Exponential

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *now = NULL;
-	unsigned int v;
+	unsigned int v = 0;
 	listint_t *past = *head;
 
 	if (*head == NULL)
