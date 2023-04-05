c23f135781d7aa4d6e1d872d851dbd7e2e4ea163
LiterallyEssie325 <olowoyeyeesther@gmail.com>
2023-04-04 17:47:59 -0700

    For the nth timegit add .

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * *get_nodeint_at_index - returns the nth node of a linked list
+ * @head: input value
+ * @index: input value
+ *
+ * Return: the nth node otherwise, NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int r = 0;
+	listint_t *cute = head;
+
+	while (cute && r < index)
+	{
+		cute = cute->next
+			r++;
+	}
+	return (cute ? cute : NULL);
+}
